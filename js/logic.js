c19ddb1cc2765b340f8c49c13afde495ef70b141
Hassan Al-najjar <ha77an19@gmail.com>
2020-02-04 15:09:28 +0200

    formatted files

--- js/logic.js
+++ js/logic.js
@@ -1,10 +1,10 @@
 function sendRequest(url, cbllback) {
-    let xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        cbllback(JSON.parse(xhr.responseText));
-      }
-    };
-    xhr.open("GET", url, true);
-    xhr.send();
+  let xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      cbllback(JSON.parse(xhr.responseText));
+    }
+  };
+  xhr.open('GET', url, true);
+  xhr.send();
 }
